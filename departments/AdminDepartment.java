374054f299bf91cd2db5a7aa36af45f9710e643d
NITESH NAINWAL <99165816+nimi23w@users.noreply.github.com>
2022-03-27 15:10:45 +0530

    Add files via upload

---
+++ departments/AdminDepartment.java
@@ -0,0 +1,17 @@
+package com.gl.deptdetails.departments;
+
+public class AdminDepartment extends SuperDepartment {
+
+	public String departmentName() {
+		return "Admin Department";
+	}
+
+	public String getTodaysWork() {
+		return "Complete your documents submission";
+	}
+
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+
+}
